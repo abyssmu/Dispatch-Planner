41036757b212cf69615e691bc3a30b4e980dd278
abyssmu <sarobin2@illinois.edu>
2022-03-22 22:59:12 -0500

    Fixed formatting and url for locations

--- js/locations.js
+++ js/locations.js
@@ -1,6 +1,6 @@
 async function getLocations()
 {
-    fetchURL = "https://abyssmu.github.io/Dispatch-Planner/locations.json"
+    fetchURL = "https://abyssmu.github.io/Dispatch-Planner/js/locations.json"
     locations = await fetch(fetchURL, {method: 'GET'}).then(function(response) {return response.json()})
     
     for(var i = 0; i < locations['locations'].length; ++i)

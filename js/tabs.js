26c47f44b02df5caeb46a73af4d95deed82cdba8
abyssmu <abyssmu@hotmail.com>
2022-04-26 12:16:35 -0500

    Working on home page.

--- js/tabs.js
+++ js/tabs.js
@@ -17,21 +17,8 @@ function openTab(name, element)
     }
 
     //Get tabcontent by id name and set display to visible
-    document.getElementById(name).style.display = "block";
+    document.getElementById(name).style.display = "flex";
 
     //Set current tablink to "active" background color
     element.style.backgroundColor = "green";
-}
-
-function hideTabs()
-{
-    //Get all tablinks and set their bg colors default
-    tabLinks = document.getElementsByClassName("tabLink");
-    for(i = 0; i < tabLinks.length; ++i)
-    {
-        if(tabLinks[i].id != "home")
-        {
-            tabLinks[i].style.backgroundColor = "";
-        }
-    }
 }
